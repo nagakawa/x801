605be09422fa461cb0226804f9d4d6895576cd9b
bluebear94 <tkook11@gmail.com>
2017-02-11 17:42:04 -0500

    Fix bug in compile-map.py

--- tools/compile-map.py
+++ tools/compile-map.py
@@ -6,6 +6,7 @@ import array
 import fparser
 import io
 import maprules
+import zlib
 
 def writeInt(f, n, b):
   f.write((n % (1 << 8 * b)).to_bytes(b, byteorder='little'))
@@ -18,10 +19,13 @@ parser.add_argument('source', metavar='source', type=str, nargs=1,
     help='the source path')
 parser.add_argument('destination', metavar='destination', type=str, nargs=1,
     help='the destination path')
+parser.add_argument('--level', metavar='L', type=int, nargs='?',
+    help='the level of compression (-1 is default; higher numbers indicate smaller size but longer compression times)')
 
 args = parser.parse_args()
 source = open(args.source[0], "r")
 dest = open(args.destination[0], "wb")
+level = args.level if args.level else -1
 
 header, sections, dsCount = fparser.parse(source)
 
@@ -33,5 +37,20 @@ writeInt(dest, dsCount, 4)
 
 for name, seclist in sections.items():
   for sec in seclist:
+    bname = name.encode()
+    if len(bname) != 4:
+      fparser.error("Section names must be 4 bytes long. You put" + name)
+    dest.write(bname)
     buf = maprules.handlers[name](sec)
-    dest.write(buf)
+    usize = len(buf)
+    cbuf = zlib.compress(buf, level)
+    csize = len(cbuf)
+    if csize + 4 < usize:
+      writeInt(dest, csize | 0x80000000, 4)
+      writeInt(dest, usize, 4)
+      writeInt(dest, zlib.adler32(cbuf), 4)
+      dest.write(cbuf)
+    else:
+      writeInt(dest, usize, 4)
+      writeInt(dest, zlib.adler32(buf), 4)
+      dest.write(buf)
