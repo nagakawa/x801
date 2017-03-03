e30a19d1b72acec4587ddfe85096a6db43b8c7ff
bluebear94 <tkook11@gmail.com>
2017-03-02 20:24:06 -0500

    Add script to update index

---
+++ tools/rebuild-index.py
@@ -0,0 +1,30 @@
+# Rebuild asset indices
+# Usage: python3 rebuild-index.py in the project root
+
+import io
+import json
+import pathlib
+
+index = {}
+
+try:
+  stream = open("assets/index.json")
+  index = json.loads(stream.readall())
+except Exception:
+  pass
+
+# An index is a dictionary from filename to a structure:
+# "filename" : { "timestamp": 9583, "version": 2 }, ...
+
+for path in pathlib.Path("assets").glob("**/*"):
+  if str(path) == "assets/index.json" or not path.is_file(): continue
+  entry = { "timestamp": 0, "version": -1 }
+  try: entry = index[str(path)]
+  except: pass
+  newtime = path.stat().st_mtime
+  if newtime != entry["timestamp"]:
+    newentry = { "timestamp": newtime, "version": entry["version"] + 1 }
+    index[str(path)] = newentry
+
+stream = open("assets/index.json", mode="w")
+stream.write(json.dumps(index))
