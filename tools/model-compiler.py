ea8e90e80ea61226a3c390fc3e548f1ed391190f
bluebear94 <tkook11@gmail.com>
2017-03-25 18:45:03 -0400

    Model compiler now produces compiled model functions instead of full models.
    This change was needed as to prevent redundant model data across
    blocks with identical models.

--- tools/model-compiler.py
+++ tools/model-compiler.py
@@ -45,6 +45,13 @@ def resolve(vertex):
     if type(vertex[i]) is str:
       vertex[i] = evaluator.eval(vertex[i])
 
+def resolve2(vertex, vlookup):
+  for i in range(len(vertex)):
+    if i == 0:
+      vertex[i] = vlookup[vertex[i]]
+    elif type(vertex[i]) is str:
+      vertex[i] = evaluator.eval(vertex[i])
+
 def parseTri(section, triangles, table, parameters):
   occlusionFlags = getDirectionFlags(section['Occlusion'][0])
   pt = False
@@ -55,22 +62,23 @@ def parseTri(section, triangles, table, parameters):
   except IndexError:
     pass
   texSym = section['Texture'][0][0]
-  texPath = parameters[texSym]
-  texPos = table[texPath]
+  ti = -1
+  if texSym in texlookup:
+    ti = texlookup[texSym]
+  else:
+    ti = len(texlookup)
+    texlookup[texSym] = ti
   v1 = section['Vertex1'][0]
   v2 = section['Vertex2'][0]
   v3 = section['Vertex3'][0]
-  resolve(v1)
-  resolve(v2)
-  resolve(v3)
-  v1[4] += texPos
-  v2[4] += texPos
-  v3[4] += texPos
+  resolve2(v1, vlookup)
+  resolve2(v2, vlookup)
+  resolve2(v3, vlookup)
   triangles += [
-    (v1, v2, v3, occlusionFlags, pt),
+    (v1, v2, v3, ti, occlusionFlags, pt),
   ]
 
-def parseQuad(section, triangles, table, parameters):
+def parseQuad(section, triangles, vlookup, texlookup):
   occlusionFlags = getDirectionFlags(section['Occlusion'][0])
   pt = False
   try:
@@ -80,63 +88,63 @@ def parseQuad(section, triangles, table, parameters):
   except IndexError:
     pass
   texSym = section['Texture'][0][0]
-  texPath = parameters[texSym]
-  texPos = table[texPath]
+  ti = -1
+  if texSym in texlookup:
+    ti = texlookup[texSym]
+  else:
+    ti = len(texlookup)
+    texlookup[texSym] = ti
   v1 = section['Vertex1'][0]
   v2 = section['Vertex2'][0]
   v3 = section['Vertex3'][0]
   v4 = section['Vertex4'][0]
-  resolve(v1)
-  resolve(v2)
-  resolve(v3)
-  resolve(v4)
-  v1[4] += texPos
-  v2[4] += texPos
-  v3[4] += texPos
-  v4[4] += texPos
+  resolve2(v1, vlookup)
+  resolve2(v2, vlookup)
+  resolve2(v3, vlookup)
+  resolve2(v4, vlookup)
   triangles += [
-    (v1, v2, v3, occlusionFlags, pt),
-    (v3, v4, v1, occlusionFlags, pt),
+    (v1, v2, v3, ti, occlusionFlags, pt),
+    (v3, v4, v1, ti, occlusionFlags, pt),
   ]
 
 parser = argparse.ArgumentParser(description='Stitch textures for Experiment801.')
-parser.add_argument('sourceApplication', metavar='sa', type=str, nargs=1,
-    help='the model application file')
-parser.add_argument('sourceTable', metavar='sd', type=str, nargs=1,
-    help='the texture location table')
-parser.add_argument('sourceFunctions', metavar='sf', type=str, nargs=1,
-    help='the path of the directory of source functions')
+parser.add_argument('sourceFunction', metavar='sf', type=str, nargs=1,
+    help='the model function source')
 parser.add_argument('output', metavar='out', type=str, nargs=1,
     help='the path of the model')
 
 args = parser.parse_args()
 
-table = readtable.read(args.sourceTable[0])
-mda = fparser.parse(open(args.sourceApplication[0]))
-mdf = fparser.parse(open(args.sourceFunctions[0] + '/' + mda[0]['Function'][0][0] + '.mdf'))
+mdf = fparser.parse(open(args.sourceFunction[0]))
 
 out = open(args.output[0], "wb")
 
 pp = pprint.PrettyPrinter(indent=1, compact=True)
 
-parameters = {}
-for key, value in mda[1]['Parameters'][0].items():
-  v = value[0]
-  if v[0] == 'sc':
-    parameters[key] = v[1]
-  else:
-    parameters[key] = v[1:]
-
 hitboxType = hitboxTypes[mdf[0]['Hitbox'][0][0]]
 opacityFlags = getDirectionFlags(mdf[0]['Opaque'][0])
+
+vertices = []
+verticesByName = {}
+mdfVertices = mdf[1]['Vertices'][0]['Vertex']
+
+for i in range(len(mdfVertices)):
+  v = mdfVertices[i]
+  name = v[0]
+  vertex = v[1:]
+  resolve(vertex)
+  vertices += [vertex]
+  verticesByName[name] = i
 
 triangles = []
 mdfFaces = mdf[1]['Faces'][0]
 
+texlookup = {}
+
 for q in mdfFaces['Quad']:
-  parseQuad(q, triangles, table, parameters)
+  parseQuad(q, triangles, verticesByName, texlookup)
 for t in mdfFaces['Triangle']:
-  parseTriangle(t, triangles, table, parameters)
+  parseTriangle(t, triangles, verticesByName, texlookup)
 
 def writeInt(f, n, b):
   f.write((n % (1 << 8 * b)).to_bytes(b, byteorder='little'))
@@ -146,19 +154,22 @@ def writeIntSigned(f, n, b):
 
 writeInt(out, hitboxType, 2)
 writeInt(out, opacityFlags, 1)
+writeInt(out, len(texlookup), 1)
+writeInt(out, len(vertices), 2)
 writeInt(out, len(triangles), 2)
 
-def writeVertex(out, v):
+for v in vertices:
   writeIntSigned(out, int(v[0] * 128), 1)
   writeIntSigned(out, int(v[1] * 128), 1)
   writeIntSigned(out, int(v[2] * 128), 1)
-  writeInt(out, int(v[3] * 128), 1)
-  writeInt(out, int(v[4] * 128), 4)
 
 for t in triangles:
-  writeVertex(out, t[0])
-  writeVertex(out, t[1])
-  writeVertex(out, t[2])
-  writeInt(out, t[3] | (t[4] << 6), 1)
+  for i in range(3):
+    writeInt(out, t[i][0], 2)
+  for i in range(3):
+    writeInt(out, t[i][1], 1)
+    writeInt(out, t[i][2], 1)
+  writeInt(out, t[3], 1)
+  writeInt(out, t[4] | (t[5] << 1), 1)
 
 out.close()
