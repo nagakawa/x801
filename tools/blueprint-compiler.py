8debe84637383955fe3a3df8b5890dc30f91431b
bluebear94 <uruwi@protonmail.com>
2017-07-21 19:23:46 -0400

    Blueprint compiler

---
+++ tools/blueprint-compiler.py
@@ -0,0 +1,89 @@
+# Takes an emc and creates an entity blueprint.
+# Usage: python3 tools/model-compiler.py
+#   <something.emc> <something.ebp>
+
+import argparse
+from collections import *
+import fparser
+import pathlib
+import pprint
+from pyquaternion import Quaternion
+from qhelper import *
+import re
+import simpleeval
+import struct
+from PIL import Image
+
+def error(msg):
+  raise InputException(msg)
+
+def writeInt(f, n, b):
+  f.write((n % (1 << 8 * b)).to_bytes(b, byteorder='little'))
+
+def writeIntSigned(f, n, b):
+  f.write((n).to_bytes(b, byteorder='little', signed=True))
+
+def writeFloat(f, x):
+  f.write(struct.pack('>f', x))
+
+def writeString16(f, s):
+  b = s.encode()
+  writeInt(f, len(b), 2)
+  f.write(b)
+
+parser = argparse.ArgumentParser(description='Compile entity blueprints for x801.')
+parser.add_argument('sourceBP', metavar='sb', type=str, nargs=1,
+    help='the blueprint source')
+parser.add_argument('output', metavar='out', type=str, nargs=1,
+    help='the path of the compiled blueprint')
+
+args = parser.parse_args()
+
+emc = fparser.parse(open(args.sourceBP[0]))
+
+emcParts = emc[1]["Parts"][0]["Part"]
+parentsByName = {}
+parts = []
+
+for i in range(0, len(emcParts)):
+  part = emcParts[i]
+  partName = part["PartName"][0][0]
+  partID = partName
+  if "PartID" in part:
+    partID = part["PartID"][0][0]
+  parentsByName[partID] = i
+
+for part in emcParts:
+  partName = part["PartName"][0][0]
+  partID = partName
+  if "PartID" in part:
+    partID = part["PartID"][0][0]
+  offsetCoordinates = part["OffsetCoordinates"][0]
+  offsetAngle = quaternionFromCommands(part["OffsetAngle"][0])
+  texture = part["Texture"][0][0]
+  parentID = part["Parent"][0][0]
+  parent = 0xFFFFFFFF if parentID == "" else parentsByName[parentID]
+  parts.append((
+    partName,
+    partID,
+    texture,
+    parent,
+    offsetAngle,
+    offsetCoordinates
+  ))
+
+out = open(args.output[0], "wb")
+
+# Header
+writeInt(out, len(parts), 4)
+
+# Parts
+for part in parts:
+  writeString16(out, part[0])
+  writeString16(out, part[1])
+  writeString16(out, part[2])
+  writeInt(out, part[3], 4)
+  for i in range(4):
+    writeFloat(out, part[4][i])
+  for i in range(3):
+    writeFloat(out, part[5][i])
--- tools/part-compiler.py
+++ tools/part-compiler.py
@@ -8,6 +8,7 @@ import fparser
 import pathlib
 import pprint
 from pyquaternion import Quaternion
+from qhelper import *
 import re
 import simpleeval
 import struct
@@ -135,44 +136,6 @@ def parseCubeRanged(section, triangles, componentIndicesByName):
     axis = section['UV' + s][0]
     uvAxes.append(axis)
   parseCubeHelper(triangles, vertexXYZ, offset, uvAxes)
-
-def quaternionFromCommands(comm):
-  i = 0
-  n = len(comm)
-  q = Quaternion()
-  while i < n:
-    label = comm[i]
-    i += 1
-    # if we have "identity", then we need not do anything
-    if label == 'identity': continue
-    flag = -1
-    # axis rotations: this is degrees ccw, looking from
-    # origin toward the positive end of the appropriate axis
-    if label == 'aroundXAxis': flag = 0
-    if label == 'aroundYAxis': flag = 1
-    if label == 'aroundZAxis': flag = 2
-    if flag == -1:
-      fparser.error("Unknown label " + str(label) + "! Check docs for details.")
-    # These commands take one argument; gobble that up
-    if i >= n: # no more entries
-      fparser.error("Reached end of argument list!")
-    angle = 0
-    try:
-      angle = float(comm[i])
-    except Exception:
-      fparser.error(comm[i] + " is not a number")
-    i += 1
-    axis = [
-      [1, 0, 0],
-      [0, 1, 0],
-      [0, 0, 1]
-    ][flag]
-    # the argument is the rotation CCW when our line of sight is parallel
-    # to the positive axis, so CW when it's pointing toward negative
-    rotation = Quaternion(axis=axis, degrees=-angle)
-    # NOTE: the order matters!
-    q = (rotation * q).normalised
-  return q
 
 def writeInt(f, n, b):
   f.write((n % (1 << 8 * b)).to_bytes(b, byteorder='little'))
