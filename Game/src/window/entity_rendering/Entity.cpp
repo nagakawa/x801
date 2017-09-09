c87c1f8da9aa5345329a229c4eec269de32e0a48
bluebear94 <uruwi@protonmail.com>
2017-09-09 12:16:53 -0400

    Add NEW Entity ABC, with an implementation for players

--- Game/src/window/entity_rendering/Entity.cpp
+++ Game/src/window/entity_rendering/Entity.cpp
@@ -22,106 +22,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace x801 {
   namespace game {
-    // Here be dragons
-    void Entity::updateComponentOfPart(
-        size_t partID, size_t componentID,
-        std::vector<bool>& isUpdated
-    ) {
-      // No need to update again
-      if (isUpdated[componentID]) return;
-      std::vector<glm::vec3>& positions =
-        absoluteComponentPositions[partID];
-      std::vector<glm::quat>& orientations =
-        absoluteComponentOrientations[partID];
-      const x801::map::Part& part = *(usedParts[partID]);
-      const x801::map::Component& comp = part.components.at(componentID);
-      size_t parentID = comp.parent;
-      // Update the component's parent first
-      if (parentID != -1U) {
-        updateComponentOfPart(partID, parentID, isUpdated);
-      } else {
-        // The parent is ground, which has no offset or rotation.
-        positions[componentID] = comp.offsetCoordinates;
-        orientations[componentID] = comp.offsetAngle;
-        return;
+    static size_t playerTexID = -1U;
+    size_t PlayerEntity::getTexture() {
+      if (playerTexID == -1U) {
+        // Get player tex ID
+        playerTexID = tb->getTexID("placeholder");
       }
-      // The absolute position is the position of the parent
-      // plus the component offset adjusted for its orientation.
-      glm::vec3 absoluteOffset =
-        orientations[parentID] * comp.offsetCoordinates;
-      positions[componentID] = positions[parentID] + absoluteOffset;
-      // The absolute orientation is the orientation of a parent
-      // influenced by control angles.
-      const std::vector<std::string>& controlNames =
-        part.controlAnglesByComponent.at(componentID);
-      glm::quat absoluteOrientation = orientations[parentID];
-      for (const std::string& name : controlNames) {
-        auto it = controlAngles.find(name);
-        if (it == controlAngles.end()) continue;
-        absoluteOrientation = it->second * absoluteOrientation;
-      }
-      orientations[componentID] = absoluteOrientation;
-    } // What a breather!
-    // Update all of the components of a part
-    void Entity::updateComponentsOfPart(size_t partID) {
-      const x801::map::Part& part = *(usedParts[partID]);
-      std::vector<bool> isUpdated(part.components.size(), false);
-      for (size_t i = 0; i < part.components.size(); ++i) {
-        updateComponentOfPart(partID, i, isUpdated);
-      }
+      return playerTexID + l.rot + (walkFrame % 2) * 4;
     }
-    void Entity::updatePartTraits(
-        size_t partID, std::vector<bool>& isUpdated) {
-      if (isUpdated[partID]) return;
-      const PartLink& link = links[partID];
-      size_t parentID = link.parentPart;
-      if (parentID != -1U) {
-        updatePartTraits(parentID, isUpdated);
-      } else { // No parent
-        absolutePartPositions[partID] = link.offset;
-        absolutePartOrientations[partID] = link.orientation;
-      }
-      // (Somewhat) copied from updateComponentOfPart
-      // Set position
-      glm::vec3 absoluteOffset =
-        absolutePartOrientations[parentID] *
-        (absoluteComponentPositions[parentID][link.component] + link.offset);
-      absolutePartPositions[partID] =
-        absolutePartPositions[parentID] + absoluteOffset;
-      // Set rotation
-      absolutePartOrientations[partID] =
-        absoluteComponentOrientations[parentID][link.component] *
-        link.orientation *
-        absolutePartOrientations[parentID];
-    }
-    void Entity::updatePartTraits() {
-      std::vector<bool> isUpdated(usedParts.size(), false);
-      for (size_t i = 0; i < usedParts.size(); ++i) {
-        updatePartTraits(i, isUpdated);
-      }
-    }
-    bool Entity::update() {
-      bool dirtyOld = dirty;
-      dirty = false;
-      // Update part positions and orientations
-      for (size_t i = 0; i < usedParts.size(); ++i)
-        updateComponentsOfPart(i);
-      updatePartTraits();
-      return dirtyOld;
-    }
-  }
-}
-
-x801::game::Entity::Entity(
-    PartView& pv,
-    const x801::map::Blueprint& bp) {
-  for (const x801::map::Blueprint::Elem& elem : bp.elems) {
-    PartLink link = {
-      elem.parent,
-      elem.component,
-      elem.offset,
-      elem.angle
-    };
-    addPart(pv.getPart(elem.name), elem.textureName, link, elem.id);
   }
 }
